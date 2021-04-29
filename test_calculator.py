1280782665c96edfd61b3695693af116481b80ac
Cole Theodore <theodoco@oregonstate.edu>
2021-04-29 16:58:01 -0700

    Cleaned up file and added brief comments

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,5 @@
+##To run, enter 'py test_calculator.py'
+
 import unittest
 import calculator
 
@@ -5,36 +7,32 @@ class Testing(unittest.TestCase):
     
 ##Addition
     def test_addition(self):    ##This test will pass
-        result = calculator.addition(5, 5)
-        self.assertEqual(result, 10)
+        self.assertEqual(calculator.addition(5, 5), 10)
 
     def test_addition2(self):    ##This test will fail
-        result = calculator.addition(5, 10)
-        self.assertEqual(result, 7)
+        self.assertEqual(calculator.addition(5, 10), 7)
 
 ##Subtraction
     def test_sub(self):         ##This test will pass
-        result = calculator.sub(10, 5)
-        self.assertEqual(result, 5)
+        self.assertEqual(calculator.sub(10, 5), 5)
 
     def test_sub2(self):         ##This test will fail
-        result = calculator.sub(10, 5)
-        self.assertEqual(result, 10)
+        self.assertEqual(calculator.sub(10, 5), 10)
 
 ##Division
     def test_divide(self):      ##This test will pass
-        result = calculator.divide(10, 5)
-        self.assertEqual(result, 2)
+        self.assertEqual(calculator.divide(10, 5), 2)
 
     def test_divide2(self):      ##This test will fail
-        result = calculator.divide(10, 0)
-        self.assertEqual(result, 2)
+        self.assertEqual(calculator.divide(10, 0), 2)
 
 ##Multiplication
     def test_multiply(self):    ##This test will pass
-        result = calculator.multiply(2, 5)
-        self.assertEqual(result, 10)
+        self.assertEqual(calculator.multiply(2, 5), 10)
 
     def test_multiply2(self):    ##This test will fail
-        result = calculator.multiply(5, 5)
-        self.assertEqual(result, 65)
+        self.assertEqual(calculator.multiply(5, 5), 65)
+
+##Clean it up a bit!
+if __name__ == "__main__":
+    unittest.main()
